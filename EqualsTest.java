d67a6f5bf34e999010ae275f7cb3ef85066d8fc3
Amar1234678 <33965185+Amar1234678@users.noreply.github.com>
2019-08-17 10:05:38 +0530

    Add files via upload

---
+++ EqualsTest.java
@@ -0,0 +1,16 @@
+class EqualsTest 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		int x=10;
+		int y=20;
+		int z=10;
+System.out.println(x==y);
+System.out.println(x==z);
+//System.out.println(x.hashCode());
+EqualsTest e=null;
+System.out.println(null.equals(null));
+	}
+}
